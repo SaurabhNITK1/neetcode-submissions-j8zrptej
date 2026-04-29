b55e336ed8cd9239cd28958386c6cd539fe64861
SaurabhNITK1 <98584702+SaurabhNITK1@users.noreply.github.com>
2026-04-29 21:20:50 +0530

    Add: reorder-linked-list - submission-7

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-7.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+       // find mid
+       ListNode slow = head, fast = head;
+
+       while (fast != null && fast.next!= null) {
+          slow = slow.next;
+          fast = fast.next.next;
+       }
+       // Slow will be at mid for odd sized list and for even sozed list also, slow will be dividing the linked list 
+       // into two halves such that left half's size is always greater than or equal to right half's size. slow.next
+       // will be the beginning of 2nd haf of array.
+
+
+       // Reverse the 2nd half.
+       ListNode prev = null;
+       ListNode curr = slow.next;
+       slow.next = null; // dereference to separate two lists
+       while (curr != null) {
+           ListNode next = curr.next;
+           curr.next = prev;
+           prev = curr;
+           curr = next;
+       }
+
+       // prev is the head of the reversed list. Now merge the 2nd list into first
+       ListNode head1 = head;         // save head of the list to be merged
+       while (prev != null) {
+           ListNode temp1 = head1.next;   // next element of list1
+           ListNode temp2 = prev.next;    // next element of list2
+        
+
+           head1.next = prev;
+           prev.next= temp1;
+
+           head1 = temp1;
+           prev = temp2;
+       }
+    }
+}
